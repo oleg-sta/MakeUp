b639591df1bffb1626e80f1b0a6f0590cd8595cf
Stadnichenko Oleg <oleg.stadnichenko@gmail.com>
2017-03-24 00:08:14 +0300

    improvements to script

--- adobescripts/AIexportToPng.jsx
+++ adobescripts/AIexportToPng.jsx
@@ -2,74 +2,61 @@
 
 #target photoshop  
 function main(){  
+var exportPath = "h:/Garbage/3/";
 if(!documents.length) return;  
 var doc = activeDocument;  
 var oldPath = activeDocument.path;  
-for(var a=0;a<doc.layers.length;a++){  
-activeDocument.activeLayer = activeDocument.layers.getByName(doc.layers[a].name);  
-//dupLayers();  
-var saveFile= File("h:/Garbage/3/"+doc.layers[a].name +".png");  
-SavePNG(saveFile);  
-//app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);  
-for (var a2 = 0; a2 < activeDocument.layers.getByName(doc.layers[a].name).groupItems.length; a2++)
-{
-   activeDocument.layers.getByName(doc.layers[a].name).groupItems[a2].hidden=true;
-}
-for (var a2 = 0; a2 < activeDocument.layers.getByName(doc.layers[a].name).pageItems.length; a2++)
-{
-   activeDocument.layers.getByName(doc.layers[a].name).pageItems[a2].hidden=true;
-}
-for (var a2 = 0; a2 < activeDocument.layers.getByName(doc.layers[a].name).groupItems.length; a2++)
-{
-   //activeDocument.layers.getByName(doc.layers[a].name).groupItems[a2].name=activeDocument.layers.getByName(doc.layers[a].name).groupItems[a2].name + "1455";
-   activeDocument.layers.getByName(doc.layers[a].name).groupItems[a2].hidden=false;
-   var gr = activeDocument.layers.getByName(doc.layers[a].name).groupItems[a2];
-   
-
-//   $.writeln ("creating new doc!"); 
-//CallFunction ("0" + gr.pageItems.length);
-var fileName = saveFile + "aa" + a2 + ".png";
-for (var a3 = 0; a3 < gr.pageItems.length; a3++)
-{
- if (typeof(gr.pageItems[a3]) != "undefined" && gr.pageItems[a3].pageItems != null) {
- var ee = gr.pageItems[a3].pageItems;
-  for (var a4 = 0; a4 < gr.pageItems[a3].pageItems.length; a4++)
+for(var a=0;a<doc.layers.length;a++) {
+  var doc1 = doc.layers[a];
+  //activeDocument.activeLayer = activeDocument.layers.getByName(doc.layers[a].name);  
+  var saveFile= File(exportPath+doc.layers[a].name +".png");  
+  SavePNG(saveFile);  
+  for (var a2 = 0; a2 < doc1.pageItems.length; a2++)
   {
-    
-//    CallFunction ("yeeee!!!e " + gr.pageItems[a3].pageItems[a4].typename);
-    if (gr.pageItems[a3].pageItems[a4].typename == "PlacedItem") {
-//        CallFunction ("yeeee!!!e " + gr.pageItems[a3].pageItems[a4].file.name);
-        fileName = "h:/Garbage/3/" + gr.pageItems[a3].pageItems[a4].file.name + ".png";
-    }
+     doc1.pageItems[a2].hidden=true;
   }
- }
- try{
- if (gr.pageItems[a3] != null && gr.pageItems[a3].typename == "PlacedItem" && gr.pageItems[a3].file != null) {
-        fileName = "h:/Garbage/3/" + gr.pageItems[a3].file.name + ".png";
- }
- } catch(e) {}
-}
-
-
-   SavePNG(fileName);  
-   activeDocument.layers.getByName(doc.layers[a].name).groupItems[a2].hidden=true;
-}
-
-
-    }  
+  for (var a2 = 0; a2 < doc1.groupItems.length; a2++)
+  {
+    doc1.groupItems[a2].hidden=false;
+    var gr = doc1.groupItems[a2];
+   
+    var fileName = saveFile + "aa" + a2 + ".png";
+    for (var a3 = 0; a3 < gr.pageItems.length; a3++)
+    {
+      // getting name for elements: eyelashes, eyeline, eyeshadow
+      if (typeof(gr.pageItems[a3]) != "undefined" && gr.pageItems[a3].pageItems != null) {
+        var ee = gr.pageItems[a3].pageItems;
+        for (var a4 = 0; a4 < gr.pageItems[a3].pageItems.length; a4++)
+        {
+//          if (a2 == 1) CallFunction(gr.pageItems[a3].pageItems[a4].typename);
+          if (gr.pageItems[a3].pageItems[a4].typename == "RasterItem") {
+            try {
+              fileName = exportPath + gr.pageItems[a3].pageItems[a4].file.name + ".png";
+            } catch(e) {}
+          }
+          // exception for one
+          if (gr.pageItems[a3].pageItems[a4].typename == "GroupItem") {
+              fileName = exportPath + gr.pageItems[a3].pageItems[a4].pageItems[0].file.name + ".png";
+          }
+        }
+      }
+     // getting name for fashion
+      try {
+        if (gr.pageItems[a3] != null && gr.pageItems[a3].typename == "RasterItem" && gr.pageItems[a3].file != null) {
+          fileName = exportPath + gr.pageItems[a3].file.name + ".png";
+        }  
+        // exception for one
+        if (gr.pageItems[a3].typename == "GroupItem") {
+            fileName = exportPath + gr.pageItems[a3].pageItems[0].file.name + ".png";
+        }
+      } catch(e) {}
+    }
+    SavePNG(fileName);  
+    doc1.groupItems[a2].hidden=true;
+    }
+  }  
 }  
 main();  
-function dupLayers() {   
-    var desc143 = new ActionDescriptor();  
-        var ref73 = new ActionReference();  
-        ref73.putClass( charIDToTypeID('Dcmn') );  
-    desc143.putReference( charIDToTypeID('null'), ref73 );  
-    desc143.putString( charIDToTypeID('Nm  '), activeDocument.activeLayer.name );  
-        var ref74 = new ActionReference();  
-        ref74.putEnumerated( charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt') );  
-    desc143.putReference( charIDToTypeID('Usng'), ref74 );  
-    executeAction( charIDToTypeID('Mk  '), desc143, DialogModes.NO );  
-};  
 function SavePNG(saveFile){  
     var exportOptions = new ExportOptionsPNG24();
     var type = ExportType.PNG24;
@@ -81,6 +68,7 @@ function SavePNG(saveFile){
     app.activeDocument.exportFile( fileSpec, type, exportOptions );
 } 
 
+// for debug purposes only
 function CallFunction (Call) {  
     win = new Window ("dialog", Call);  
     win.add ("statictext", undefined, Call);  
