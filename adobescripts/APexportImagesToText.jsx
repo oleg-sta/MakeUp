55a6ea21fe1543f9b102d93d36756360573a59a8
Stadnichenko Oleg <oleg.stadnichenko@gmail.com>
2017-04-05 22:34:12 +0400

    поправлен скрипт вырезки цвета вставлены цвета для губ

--- adobescripts/APexportImagesToText.jsx
+++ adobescripts/APexportImagesToText.jsx
@@ -15,22 +15,44 @@ for(var i = 0 ; i < doc.layers.length;i++) {
     var eyeshadow = "";
     var eyelashes = "";
     var lay = doc.layers[i];
+    var tail = "";
+    var colorLips = "";
     if (lay.layers != null) {
         for(var j = 0 ; j < lay.layers.length;j++) {
 //        line += ";" + lay.layers[j].name;
           if (lay.layers[j].name.indexOf("lash") != -1) {
               eyelashes = lay.layers[j].name;
-          }
-          if (lay.layers[j].name.indexOf("line") != -1) {
+          } else if (lay.layers[j].name.indexOf("line") != -1) {
               eyeline = lay.layers[j].name;
-          }
-          if (lay.layers[j].name.indexOf("shad") != -1) {
+          } else if (lay.layers[j].name.indexOf("shad") != -1) {
               eyeshadow = lay.layers[j].name;
+          } else if (lay.layers[j].kind == "LayerKind.SOLIDFILL" && lay.layers[j].name.indexOf("ips") > 0) {
+              //var color = lay.layers[j].getObjectValue(charIDToTypeID('Clr ')); 
+              app.activeDocument.activeLayer = lay.layers[j];
+              var col = getFillColor();
+              colorLips = col.rgb.hexValue;
+          } else {
+              tail += ";" + lay.layers[j].name + "(" + lay.layers[j].kind + ")";
           }
         }
     }
-    saveFile.writeln(line + ";" + eyeline + ";" + eyelashes + ";" + eyeshadow);
+    saveFile.writeln(line + ";" + eyeline + ";" + eyelashes + ";" + eyeshadow + ";" + colorLips + tail);
 }
 
 saveFile.close();
 saveFile.close();
+
+
+function getFillColor(){
+   var ref = new ActionReference();
+   ref.putEnumerated( stringIDToTypeID( "contentLayer" ), charIDToTypeID( "Ordn" ), charIDToTypeID( "Trgt" ));
+   var ref1= executeActionGet( ref );
+   var list =  ref1.getList( charIDToTypeID( "Adjs" ) ) ;
+   var solidColorLayer = list.getObjectValue(0);        
+   var color = solidColorLayer.getObjectValue(charIDToTypeID('Clr ')); 
+   var fillcolor = new SolidColor;
+   fillcolor.rgb.red = color.getDouble(charIDToTypeID('Rd  '));
+   fillcolor.rgb.green = color.getDouble(charIDToTypeID('Grn '));
+   fillcolor.rgb.blue = color.getDouble(charIDToTypeID('Bl  '));
+   return fillcolor;
+}
