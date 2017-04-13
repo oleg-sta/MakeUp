902bca781ad9a78ba61c701092a4ab6055c9c82b
Stadnichenko Oleg <oleg.stadnichenko@gmail.com>
2017-04-13 19:54:36 +0400

    formatting to script

--- adobescripts/AIexportToPng.jsx
+++ adobescripts/AIexportToPng.jsx
@@ -1,67 +1,75 @@
 // this script for exporting specific group to png files
 
 #target photoshop  
-function main(){  
+function main() {
     var exportPath = "h:/Garbage/3/";
     if(!documents.length) return;  
     var doc = activeDocument;  
     var oldPath = activeDocument.path;  
-    for(var a=0;a<doc.layers.length;a++) {
-       var doc1 = doc.layers[a];
+    for(var a=0; a < doc.layers.length; a++) {
+        var doc1 = doc.layers[a];
       //activeDocument.activeLayer = activeDocument.layers.getByName(doc.layers[a].name);  
-        var saveFile= File(exportPath+doc.layers[a].name +".png");  
+        var saveFile = File(exportPath+doc.layers[a].name + ".png");  
         SavePNG(saveFile);  
+// make all hidden
         for (var a2 = 0; a2 < doc1.pageItems.length; a2++)
         {
-            doc1.pageItems[a2].hidden=true;
+            doc1.pageItems[a2].hidden = true;
         }
+        var prevLayer = null;
         for (var a2 = 0; a2 < doc1.groupItems.length; a2++)
         {
-            doc1.groupItems[a2].hidden=false;
+            doc1.groupItems[a2].hidden = false;
             var gr = doc1.groupItems[a2];
             var fileName = saveFile + "aa" + a2 + ".png";
             for (var a3 = 0; a3 < gr.pageItems.length; a3++)
             {
           // getting name for elements: eyelashes, eyeline, eyeshadow
-            if (typeof(gr.pageItems[a3]) != "undefined" && gr.pageItems[a3].pageItems != null) {
-            var ee = gr.pageItems[a3].pageItems;
-            for (var a4 = 0; a4 < gr.pageItems[a3].pageItems.length; a4++)
-            {
-//           if (a2 == 1) CallFunction(gr.pageItems[a3].pageItems[a4].typename);
-                if (gr.pageItems[a3].pageItems[a4].typename == "RasterItem") {
-                    try {
-                        fileName = exportPath + gr.pageItems[a3].pageItems[a4].file.name + ".png";
-                    } catch(e) {}
+                if (typeof(gr.pageItems[a3]) != "undefined" && gr.pageItems[a3].pageItems != null) {
+                    var ee = gr.pageItems[a3].pageItems;
+                    for (var a4 = 0; a4 < gr.pageItems[a3].pageItems.length; a4++)
+                    {
+       //           if (a2 == 1) CallFunction(gr.pageItems[a3].pageItems[a4].typename);
+                        if (gr.pageItems[a3].pageItems[a4].typename == "RasterItem") {
+                            try {
+                                fileName = exportPath + gr.pageItems[a3].pageItems[a4].file.name + ".png";
+                            } catch(e) {}
+                        }
+                   // exception for one
+                        if (gr.pageItems[a3].pageItems[a4].typename == "GroupItem") {
+                            fileName = exportPath + gr.pageItems[a3].pageItems[a4].pageItems[0].file.name + ".png";
+                        }
+                    }
                 }
-          // exception for one
-             if (gr.pageItems[a3].pageItems[a4].typename == "GroupItem") {
-                fileName = exportPath + gr.pageItems[a3].pageItems[a4].pageItems[0].file.name + ".png";
+                // getting name for fashion
+// if fashion we should on previous element
+                try {
+                    if (gr.pageItems[a3] != null && gr.pageItems[a3].typename == "RasterItem" && gr.pageItems[a3].file != null) {
+                        fileName = exportPath + gr.pageItems[a3].file.name + ".png";
+//                        prevLayer.hidden = false;
+                    }  
+                // exception for one
+                    if (gr.pageItems[a3].typename == "GroupItem") {
+                        fileName = exportPath + gr.pageItems[a3].pageItems[0].file.name + ".png";
+//                        prevLayer.hidden = false;
+                    }
+                } catch(e) {}
             }
-        }
-    }
-     // getting name for fashion
-    try {
-        if (gr.pageItems[a3] != null && gr.pageItems[a3].typename == "RasterItem" && gr.pageItems[a3].file != null) {
-            fileName = exportPath + gr.pageItems[a3].file.name + ".png";
-        }  
-        // exception for one
-        if (gr.pageItems[a3].typename == "GroupItem") {
-            fileName = exportPath + gr.pageItems[a3].pageItems[0].file.name + ".png";
-        }
-    } catch(e) {}
-        }
             SavePNG(fileName);  
             doc1.groupItems[a2].hidden=true;
+            if (prevLayer != null) {prevLayer.hidden = true;}
+            prevLayer = doc1.groupItems[a2];
         }
     }  
 
-    for(var a=0;a<doc.layers.length;a++) {
-       var doc1 = doc.layers[a];
+// make all visible
+    for (var a=0; a < doc.layers.length; a++) {
+        var doc1 = doc.layers[a];
         var saveFile= File(exportPath+doc.layers[a].name +".png");  
         SavePNG(saveFile);  
         for (var a2 = 0; a2 < doc1.pageItems.length; a2++)
         {
-            doc1.pageItems[a2].hidden=false;
+            doc1.pageItems[a2].hidden = false;
         }
     }
 
