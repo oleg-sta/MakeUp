5b576ef85ead08baa07bf62f6f5340bea4a2a96c
Stadnichenko Oleg <oleg.stadnichenko@gmail.com>
2017-05-06 08:34:33 +0400

    mirror more rounded with lense effect outside

--- assets/shaders/fss_no_face.glsl
+++ assets/shaders/fss_no_face.glsl
@@ -16,11 +16,11 @@ vec4 mainImage(vec2 fragCoord, vec4 WebCamPixelAt)
     float y0 = 0.5;
 
     //Large elipse
-    float width_ellipse_1 = 0.5;
+    float width_ellipse_1 = 0.6;
     float height_ellipse_1 = 0.7;
 
     //Small ellipse
-    float width_ellipse_2 = 0.49;
+    float width_ellipse_2 = 0.587;
     float height_ellipse_2 = 0.686;
 
 
@@ -28,20 +28,20 @@ vec4 mainImage(vec2 fragCoord, vec4 WebCamPixelAt)
     //If we are outside the large ellipse
     if (4.0*(uv.x - x0)*(uv.x - x0)/(width_ellipse_1*width_ellipse_1) + 4.0*(uv.y - y0)*(uv.y - y0)/(height_ellipse_1*height_ellipse_1) > 1.0)
     {
-        fragColor = mix(WebCamPixelAt, vec4(0.1, 0.0, 0.0, 1.0), 0.4);
+        vec2 newCoor = vec2((uv.x - x0) / 1.2 + x0, (uv.y - y0) / 1.2 + y0);
+        fragColor = mix(texture2D(sTexture, newCoor), vec4(0.1, 0.0, 0.0, 1.0), 0.6);
     }
     else
     {
         //Check that we are inside small ellipse
-
-
         if(4.0*(uv.x - x0)*(uv.x - x0)/(width_ellipse_2*width_ellipse_2) + 4.0*(uv.y - y0)*(uv.y - y0)/(height_ellipse_2*height_ellipse_2) < 1.0)
         {
             fragColor = WebCamPixelAt;
         }
         else
         {
-            fragColor = mix(WebCamPixelAt, vec4(1.0, 1.0, 1.0, 1.0), 1.0);
+            // Border of mirror
+            fragColor = vec4(1.0, 1.0, 1.0, 1.0);
         }
 
     }
